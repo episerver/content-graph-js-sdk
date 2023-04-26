9062ba6bbf9b661a5a4c27421f06c20dcbfe674f
Thanh Nguyen <thanh.nguyen@episerver.com>
2023-04-26 08:44:51 +0700

    CG-2636: Automating Deployment of Decoupled Backend Site to Azure App Service (#14)
    ## Description
    This pull request adds the necessary Azure infrastructure for our
    project, including the deployment of the Azure App Service, Azure
    Storage Account, Azure Container Registry (ACR), and Azure SQL Server.
    
    In addition, a Docker image for the decoupled site backend has been
    built and pushed to the ACR, allowing for easy deployment and scaling of
    the backend service.
    
    ## Changes Made
    * Added Azure infrastructure deployment scripts for App Service, Storage
    Account, ACR, and SQL Server
    * Built Docker image for decoupled site backend and pushed it to ACR
    
    ## Testing
    The changes have been tested by running the GitHub action manually to
    ensure proper functionality.
    
    ![image](https://user-images.githubusercontent.com/8802680/234166262-14c156c8-18c4-4c63-82ed-14825bd84017.png)
    
    ![image](https://user-images.githubusercontent.com/8802680/234166290-43f39785-11c1-4d85-851f-03f431cf3a4a.png)
    
    Please review and let me know if you have any questions or feedback.
    Thank you.
    
    ---------
    
    Co-authored-by: Quang Tran <quang.tran@episerver.com>

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,15 +1,3 @@
-variable "subscription_id" {
-    default = ""
-}
-variable "client_id" {
-    default = ""
-}
-variable "client_secret" {
-    default = ""
-}
-variable "tenant_id" {
-    default = ""
-}
 provider "azurerm" {
     features {}
     subscription_id = "${var.subscription_id}"
@@ -32,7 +20,7 @@ resource "azurerm_container_registry" "musicfestival" {
 }
 
 resource "azurerm_storage_account" "musicfestival" {
-  name                     = "backendstorage"
+  name                     = "musicfestivalsa"
   resource_group_name      = azurerm_resource_group.musicfestival.name
   location                 = azurerm_resource_group.musicfestival.location
   account_tier             = "Standard"
@@ -45,7 +33,14 @@ resource "azurerm_mssql_server" "musicfestival" {
   location                     = azurerm_resource_group.musicfestival.location
   version                      = "12.0"
   administrator_login          = "sqladmin"
-  administrator_login_password = "P@ssw0rd1234"
+  administrator_login_password = "${var.sql_password}"
+}
+
+resource "azurerm_mssql_firewall_rule" "internal" {
+  name             = "internal"
+  server_id        = azurerm_mssql_server.musicfestival.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
 }
 
 resource "azurerm_mssql_database" "musicfestival" {
@@ -63,14 +58,14 @@ resource "azurerm_service_plan" "musicfestival" {
 }
 
 resource "azurerm_linux_web_app" "musicfestival" {
-  name                = "backendappservice"
+  name                = "musicfestival-app"
   location            = azurerm_resource_group.musicfestival.location
   resource_group_name = azurerm_resource_group.musicfestival.name
   service_plan_id     = azurerm_service_plan.musicfestival.id
 
   site_config {
       application_stack {
-          docker_image     = "${azurerm_container_registry.musicfestival.login_server}/musicfestivalimage"
+          docker_image     = "${azurerm_container_registry.musicfestival.login_server}/musicfestival"
           docker_image_tag = "latest"
       }
     always_on        = true
@@ -81,6 +76,14 @@ resource "azurerm_linux_web_app" "musicfestival" {
     "DOCKER_REGISTRY_SERVER_URL"         = "https://${azurerm_container_registry.musicfestival.login_server}"
     "DOCKER_REGISTRY_SERVER_USERNAME"    = azurerm_container_registry.musicfestival.admin_username
     "DOCKER_REGISTRY_SERVER_PASSWORD"    = azurerm_container_registry.musicfestival.admin_password
+    "ConnectionStrings__EPiServerDB"     = "Server=tcp:${azurerm_mssql_server.musicfestival.name}.database.windows.net,1433;Initial Catalog=${azurerm_mssql_database.musicfestival.name};Persist Security Info=False;User ID=${azurerm_mssql_server.musicfestival.administrator_login};Password=${azurerm_mssql_server.musicfestival.administrator_login_password};MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+    "Optimizely__ContentGraph__AllowSendingLog"       = true
+    "Optimizely__ContentGraph__AllowSyncDraftContent" = true
+    "Optimizely__ContentGraph__Experimental"          = true
+    "Optimizely__ContentGraph__GatewayAddress"        = "${var.gateway}"
+    "Optimizely__ContentGraph__AppKey"                = "${var.app_key}"
+    "Optimizely__ContentGraph__Secret"                = "${var.secret}"
+    "Optimizely__ContentGraph__SingleKey"             = "${var.single_key}"
   }
 }
 
