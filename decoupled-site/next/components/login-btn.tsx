4859337832c8f7d1875483090a03f7568f1c302f
Quang Tran <quang.tran@episerver.com>
2023-04-27 17:40:01 +0700

    wip

--- decoupled-site/next/components/login-btn.tsx
+++ decoupled-site/next/components/login-btn.tsx
@@ -1,7 +1,8 @@
-import { useSession, signIn, signOut } from "next-auth/client"
+// import { useSession, signIn, signOut } from "next-auth/client"
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Component() {
-  const [ session ] = useSession()
+  const { data: session}= useSession()
   if (session) {
     return (
       <div className="btn">
