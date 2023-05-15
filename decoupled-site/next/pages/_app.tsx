a15554208b1ef04201de4b8f69d190f5068df85a
Okan Cetin <okan.cetin@episerver.com>
2023-05-15 15:19:19 +0200

    CG-4067: Integrate with appInsights

--- decoupled-site/next/pages/_app.tsx
+++ decoupled-site/next/pages/_app.tsx
@@ -1,14 +1,30 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import NextApp from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { withApplicationInsights } from 'next-applicationinsights';
 
 
 const client = new QueryClient();
 
-export default function App({ Component, pageProps: { session, ...pageProps} }: AppProps) {
-  return (
-    <QueryClientProvider {...session} client={client}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
-  )
+class App extends NextApp {
+  render() {
+    const { Component, pageProps: { session, ...pageProps } } = this.props
+
+    return (
+      <QueryClientProvider {...session} client={client}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    )
+  }
 }
+
+const Component = process.env.NEXT_PUBLIC_APPINSIGHTS_CONN_STR
+  ? withApplicationInsights({
+    connectionString: process.env.NEXT_PUBLIC_APPINSIGHTS_CONN_STR,
+    isEnabled: process.env.NODE_ENV === 'production'
+  })(App)
+  : App
+
+export default Component
+
+
