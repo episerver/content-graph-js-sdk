b5be98ec6e5725679ffb21725422e50687a5afab
bruce ng <brucengdev@gmail.com>
2023-06-07 17:21:10 +0700

    Moved setup.cmd to backend site folder

---
+++ decoupled-site/backend/setup.cmd
@@ -0,0 +1,17 @@
+@ECHO OFF
+SETLOCAL
+
+SET BASE=.\App_Data
+
+ECHO Removed all files from the App_Data folder
+IF EXIST %BASE%\blobs\ RMDIR %BASE%\blobs\ /S/Q || EXIT /B 1
+IF EXIST %BASE%\musicfestival.mdf DEL %BASE%\musicfestival.mdf /F/Q || EXIT /B 1
+IF EXIST %BASE%\musicfestival_log.ldf DEL %BASE%\musicfestival_log.ldf /F/Q || EXIT /B 1
+
+ECHO Created new blobs
+XCOPY %BASE%\blobs_default %BASE%\blobs /Y/E/C/I || EXIT /B 1
+
+ECHO Created new database
+XCOPY %BASE%\db.mdf %BASE%\musicfestival.mdf* /Y/C || EXIT /B 1
+
+EXIT /B %ERRORLEVEL%
