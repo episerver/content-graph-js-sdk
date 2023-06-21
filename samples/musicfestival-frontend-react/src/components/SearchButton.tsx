fdbcc9ddbe61ae807f653edab6cb5ac72bc1cc58
Doan Thanh Nguyen <doanthanh.nguyen@optimizely.com>
2023-06-21 17:54:57 +0700

    CG-4370 improve autocomplete
    - made some small changes

--- samples/musicfestival-frontend-react/src/components/SearchButton.tsx
+++ samples/musicfestival-frontend-react/src/components/SearchButton.tsx
@@ -1,4 +1,3 @@
-import { Autocomplete, TextField } from "@mui/material";
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { ArtistAutocompleteQuery, useArtistAutocompleteQuery } from "../generated";
@@ -31,7 +30,7 @@ function SearchButton(): JSX.Element {
 
     function onValueChange(event: any){
         setSearchValue(event.target.value);
-        event.target.value != "" && event.target.value !== undefined ? setIsShown(true) : setIsShown(false);
+        event.target.value !== undefined ? setIsShown(true) : setIsShown(false);
     }
 
     function onAutoClick(event: any){
@@ -41,51 +40,39 @@ function SearchButton(): JSX.Element {
 
     return (
         <div>
-            <div className="nav-table-cell">
-                <input className="search-input" type="text" id="search-input" placeholder="Search" 
-                    onKeyPress={(event) => {search(event, 'keypress')}} value={searchValue} onChange={onValueChange} />
+            <div className="nav-table-cell autocomplete">
+                <input className="search-input" 
+                    type="text" 
+                    id="search-input" 
+                    placeholder="Search" 
+                    onKeyPress={(event) => {search(event, 'keypress')}} 
+                    value={searchValue} 
+                    onChange={onValueChange}
+                    onFocus={() => setIsShown(true)}
+                    onBlur={() =>
+                        setTimeout(() => {
+                            setIsShown(false);
+                        }, 150)
+                    } />
                     <a className="search-icon" onClick={(event) => {search(event, 'buttonclick')}}>
                         <i className="fa fa-search"></i>
                     </a>
-
-                    {/* <Autocomplete
-                        sx={{
-                        display: 'inline-block',
-                        '& input': {
-                            width: 200,
-                            bgcolor: 'background.paper',
-                            color: (theme) =>
-                            theme.palette.getContrastText(theme.palette.background.paper),
-                        },
-                        }}
-                        id="custom-input-demo"
-                        options={stringArr}
-                        renderInput={(params) => (
-                        <div ref={params.InputProps.ref}>
-                            <input className="search-input" type="text" id="search-input" placeholder="Search" 
-                    onKeyPress={(event) => {search(event, 'keypress')}} value={searchValue} onChange={onValueChange} {...params.inputProps} />
-                    <a className="search-icon" onClick={(event) => {search(event, 'buttonclick')}}>
-                        <i className="fa fa-search"></i>
-                    </a>
-                        </div>
-                        )}
-                    /> */}
-            <div className="autocomplete-block" style={isShown ? {display: "inherit"}: {display: "none"}}>
-                {
-                    autocompleteData?.ArtistDetailsPage?.autocomplete?.ArtistName?.map((name) => {
-                        return(
-                            <div key={name} onClick={(event) => onAutoClick(event)}>{name}</div>
-                        )
-                    })                    
-                }
-                {
-                    autocompleteData?.ArtistDetailsPage?.autocomplete?.StageName?.map((name) => {
-                        return(
-                            <div key={name} onClick={(event) => onAutoClick(event)}>{name}</div>
-                        )
-                    })
-                }
-            </div>
+                <div className="autocomplete-block" style={isShown ? {display: "inherit"}: {display: "none"}}>
+                    {
+                        autocompleteData?.ArtistDetailsPage?.autocomplete?.ArtistName?.map((name) => {
+                            return(
+                                <div key={name} onClick={(event) => onAutoClick(event)}>{name}</div>
+                            )
+                        })                    
+                    }
+                    {
+                        autocompleteData?.ArtistDetailsPage?.autocomplete?.StageName?.map((name) => {
+                            return(
+                                <div key={name} onClick={(event) => onAutoClick(event)}>{name}</div>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
     );
